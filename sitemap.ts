bd3078473cb05e7c2927097e312fd5bbfce5dc9d
correcteat <tsitakh95@gmail.com>
2026-08-22 15:22:21 +0300

    Use Vercel production URL for sitemap

--- sitemap.ts
+++ sitemap.ts
@@ -1,7 +1,10 @@
 import type { MetadataRoute } from 'next';
 
+const base = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : 'https://webpage-correcteat1.vercel.app';
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  const base = 'https://webpage-correcteat1.vercel.app';
   const now = new Date();
   const pages = ['/', '/sxetika-me-emena', '/ypiresies', '/arthra', '/epikoinonia'];
 
