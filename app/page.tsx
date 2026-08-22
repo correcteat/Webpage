9f7981e9253623693929db71cf300591bfbc0bfa
correcteat <tsitakh95@gmail.com>
2026-08-22 17:13:19 +0300

    Use Correct Eat logo for homepage article cards

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home(){
    <div className="container">
     <div className="article-list-grid">
      {featured.map(a=><Link className="article-card-list" href={`/arthra/${a.slug}`} key={a.slug}>
-      <div className="article-visual" style={{backgroundImage:`url(${a.image})`}}><span>{a.category}</span></div>
+      <div className="article-visual" style={{backgroundImage:'url(/logo.jpg)',backgroundSize:'cover',backgroundPosition:'center'}}><span>{a.category}</span></div>
       <div className="article-body"><div className="article-meta">{a.date}</div><h3>{a.title}</h3><p>{a.excerpt}</p></div>
      </Link>)}
     </div>
